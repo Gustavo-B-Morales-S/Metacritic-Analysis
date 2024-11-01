c11fa532b38e019ab5b4afe24e087b96ce8b809d
Gustavo-B-Morales-S <protected_gustavo_borges@outlook.com>
2024-11-01 17:42:03 -0300

    Added: Airflow via Astronomer CLI

---
+++ dags/pipeline_dag.py
@@ -0,0 +1,13 @@
+from airflow.decorators import dag, task
+from pendulum import datetime
+
+@dag(
+    tags=['TODO'],
+)
+# TODO
+def function():
+
+    # TODO
+    @task
+    def task() -> None:
+        pass
